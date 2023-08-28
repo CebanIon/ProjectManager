27c2271b9b2dd984782903344c48b51d8f9266b5
Ion Stroncea <ion.stroncea@iongroup.com>
2023-08-28 12:20:19 +0300

    Modified lenght error messages

--- ProjectManager.Application/Projects/Queries/ModifyProject/Validator/ModifyProjectQueryValidator.cs
+++ ProjectManager.Application/Projects/Queries/ModifyProject/Validator/ModifyProjectQueryValidator.cs
@@ -13,9 +13,9 @@ namespace ProjectManager.Application.Projects.Queries.ModifyProject.Validator
         {
             RuleFor(x => x.Id).NotEmpty().GreaterThan(0);
             RuleFor(x => x.Name).NotEmpty().WithMessage("Task name cannot be empty")
-                .MinimumLength(5).WithMessage("Task name too short");
+                .MinimumLength(5).WithMessage("Task name should contain at least 5 characters");
             RuleFor(x => x.Description).NotEmpty().WithMessage("Task description cannot be empty")
-                .MinimumLength(30).WithMessage("Task description too short");
+                .MinimumLength(30).WithMessage("Task description should contain at least 30 characterst");
             RuleFor(x => x.ProjectStateId).NotEmpty().GreaterThan(0);
         }
     }
--- ProjectManager.Application/Users/Queries/UpdateUser/Validator/UpdateUserQueryValidator.cs
+++ ProjectManager.Application/Users/Queries/UpdateUser/Validator/UpdateUserQueryValidator.cs
@@ -14,8 +14,8 @@ namespace ProjectManager.Application.Users.Queries.UpdateUser.Validator
             RuleFor(x => x.Id).NotEmpty().GreaterThan(0);
             RuleFor(x => x.LastModifiedBy).NotEmpty().GreaterThan(0);
             RuleFor(x => x.UserName).NotEmpty().WithMessage("Username cannot be empty")
-                .MinimumLength(5).WithMessage("Username to short")
-                .MaximumLength(30).WithMessage("Username to long");
+                .MinimumLength(5).WithMessage("Username should contain at least 5 characters")
+                .MaximumLength(30).WithMessage("Username should contain at maximum 30 characters");
             RuleFor(x => x.Email).EmailAddress().WithMessage("Not an email address");
             RuleFor(x => x.RoleId).NotEmpty().GreaterThan(0);
         }
